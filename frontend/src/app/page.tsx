72b43511ac5fb3f7357d60f5ed8d9d3b8ba2416d
Luís Maia <luismaia1407@gmail.com>
2024-10-01 21:45:53 -0300

    feat: product detail

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,13 @@
+import ProdutoItem from "@/components/produto/ProdutoItem";
+import { produtos } from "@/core";
 
 export default function Home() {
   return (
-    <div>
-      Aplicação
+    <div className="container grid grid-cols-4">
+      {produtos.map((produto) => (
+        <ProdutoItem  key={produto.id} produto={produto}/>
+      ))}
+
     </div>
   );
 }
